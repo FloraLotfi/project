c0f0b1574dfddf5cc7c7ece7a31335bee2570097
FloraLotfi <lotfi.farideh.e@gmail.com>
2025-01-25 15:40:30 -0800

    Update f.paria1.py

--- f.paria1.py
+++ f.paria1.py
@@ -8,7 +8,7 @@ class Node:
         child4=None
 
     def Index(self,num,p):
-        idx=self.p.index(num)
+        idx=p.index(num)
         i= idx//3
         j= idx-(i*3)
         return i,j
@@ -21,14 +21,9 @@ class Node:
     def possible_moves(self,i, j):
         S_moves = []
         possible_moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        if 0 <= i + possible_moves[0][0] < 3 and 0 <= j + possible_moves[0][1] < 3:
-            S_moves.append(possible_moves[0])
-        if 0 <= i + possible_moves[1][0] < 3 and 0 <= j + possible_moves[1][1] < 3:
-            S_moves.append(possible_moves[1])
-        if 0 <= i + possible_moves[2][0] < 3 and 0 <= j + possible_moves[2][1] < 3:
-            S_moves.append(possible_moves[2])
-        if 0 <= i + possible_moves[3][0] < 3 and 0 <= j + possible_moves[3][1] < 3:
-            S_moves.append(possible_moves[3])
+        for k in possible_moves:
+            if (0 <= i + k[0]) and (i+k[0] < 3) and (0 <= j + k[1])and (j +k[1] < 3):
+                S_moves.append(k)
         return S_moves
 
     def h(self,puzzle,goal):
@@ -39,38 +34,20 @@ class Node:
             hcost+=(abs(ig-ip)+abs(jg-jp))
         return hcost
 
-    def child1 (puzzle):#Left
-        list_puzzle=list(puzzle)
-        index0=puzzle.index('0')
-        list_puzzle[index0],list_puzzle[index0-1]=list_puzzle[index0-1],list_puzzle[index0]
-        puzzle=str(list_puzzle)
-        #call def f?????????
-        return puzzle
-        
-    def child2 (puzzle):#Right
-        list_puzzle=list(puzzle)
-        index0=puzzle.index('0')
-        list_puzzle[index0],list_puzzle[index0+1]=list_puzzle[index0+1],list_puzzle[index0]
-        puzzle=str(list_puzzle)
-        #call def f?????????
-        return puzzle
-        
-    def child1 (puzzle):#Up
-        list_puzzle=list(puzzle)
-        index0=puzzle.index('0')
-        list_puzzle[index0],list_puzzle[index0-3]=list_puzzle[index0-3],list_puzzle[index0]
-        puzzle=str(list_puzzle)
-        #call def f?????????
-        return puzzle
-        
-    def child1 (puzzle):#Down
-        list_puzzle=list(puzzle)
-        index0=puzzle.index('0')
-        list_puzzle[index0],list_puzzle[index0+3]=list_puzzle[index0+3],list_puzzle[index0]
-        puzzle=str(list_puzzle)
-        #call def f?????????
-        return puzzle
+    def children(self):
+        i0,j0=self.Index('0',self.puzzle)
+        possible=self.possible_moves(i0,j0)
+        children=[]
+        for i in possible:
+            new_puzzle=list(self.puzzle)
+            indx=new_puzzle.index('0')
+            new_puzzle[indx],new_puzzle[indx+(i[0]*3)+i[1]]=new_puzzle[indx+(i[0]*3)+i[1]],new_puzzle[indx] 
+            new_puzzle=''.join(new_puzzle)
+            children.append(new_puzzle)
+        return children
+
     
 
-f=Node("35261780","123456780")
-print(f.possible_moves(0,0))
+f=Node("123406785","123456780")
+#print(f.possible_moves(0,0))
+print(f.children())
